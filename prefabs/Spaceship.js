da1b3f4de2560a05b28f98a77b0f7a499a5eb7e6
Noel Calinawan <ncalinaw@ucsc.edu>
2020-04-13 02:56:32 -0700

    Commit

---
+++ prefabs/Spaceship.js
@@ -0,0 +1,20 @@
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+        this.points = pointValue;
+    }
+
+    update() {
+        // moves spaceship left
+        this.x -= game.settings.spaceshipSpeed;
+        // wraparound from left to right edge
+        if(this.x <= 0-this.width) {
+            this.x = game.config.width;
+        }
+    }
+
+    reset() {
+        this.x = game.config.width;
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 let config = {
     type: Phaser.CANVAS,
     width: 640,
-    height: 480
+    height: 480,
+    scene: [ Menu, Play]
 }
 
 let game = new Phaser.Game(config);
+
+game.settings = {
+    spaceshipSpeed: 3,
+    gameTImer: 60000
+}
+
+let keyF, keyLEFT, keyRIGHT;
